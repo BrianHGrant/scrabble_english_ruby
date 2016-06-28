56b724dd769cdf98a2e9dae5804a16336996745d
Brian Grant and Sky Rousse <brian@>
2016-06-28 08:53:44 -0700

    add specs for letter values

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,8 +1,23 @@
 class String
   define_method(:scrabble_score) do
     two_point = ["d","g"]
+    three_point = ["b","c","m","p"]
+    four_point = ["f","h","v","w","y"]
+    five_point = ["k"]
+    eight_point = ["j","x"]
+    ten_point = ["q","z"]
     if two_point.include?(self)
       2
+    elsif three_point.include?(self)
+      3
+    elsif four_point.include?(self)
+      4
+    elsif five_point.include?(self)
+      5
+    elsif eight_point.include?(self)
+      8
+    elsif ten_point.include?(self)
+      10
     else
       1
     end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -8,4 +8,19 @@ describe('String#scrabble_score') do
   it("returns a scrabble score of 2 for letters D or G") do
     expect("d".scrabble_score()).to(eq(2))
   end
+  it("returns a scrabble score of 3 for letters B, C, M, P ") do
+    expect("b".scrabble_score()).to(eq(3))
+  end
+  it("returns a scrabble score of 4 for letters F, H, V, W, Y  ") do
+    expect("f".scrabble_score()).to(eq(4))
+  end
+  it("returns a scrabble score of 5 for letter K") do
+    expect("k".scrabble_score()).to(eq(5))
+  end
+  it("returns a scrabble score of 8 for letters J, X") do
+    expect("j".scrabble_score()).to(eq(8))
+  end
+  it("returns a scrabble score of 10 for letters Q, Z") do
+    expect("q".scrabble_score()).to(eq(10))
+  end
 end
